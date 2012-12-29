eeaa59c7aee5fa32d16d3fd5c7ab2f95eda198b6
Shubham Lakhiwal <shubham.lakhiwal@hashedin.com>
2012-12-29 20:09:38 +0530

    Found Highest Rated Movie By Year.

--- manager.py
+++ manager.py
@@ -1,3 +1,5 @@
+from collections import namedtuple
+
 #New Field starts Here
 #Movie Manager Calculates all the WarmUp problems.
 def movie_manager():
@@ -20,6 +22,8 @@ def movie_manager():
     
     #Find Highest Rated Movie By Year
     highest_rated_movie_by_year=find_highest_rated_movie_by_year(movies_dict)
+    for line in highest_rated_movie_by_year:
+        print("Highest Rated Movie Of Year "+str(line)+" : "+str(movies_dict[highest_rated_movie_by_year[line]]))
     #Found Highest Rated Movie By Year
 
     #for line in movies_dict:
@@ -31,29 +35,39 @@ def movie_manager():
         print(str(line)+" : "+str(rating_movie_map[line]))
 #Movie Manager Ends Here.    
 
+Movie = namedtuple('Movie', ['title', 'year', 'rating'])
 #New Field Starts Here.
 #Movie Data File Handling goes here.
 def get_movie_dict():
     my_map={}
-    my_map[0]=("Toy Story",1979,4.5)
-    my_map[1]=("SpiderMan",2001,4.33)
-    my_map[2]=("Harry Potter",2006,4.2)
-    my_map[3]=("James Bond",1999,5.0)
-    my_map[4]=("Rush Hour",1999,4.5)
+    my_map[0] = Movie("Toy Story",1979,4.5)
+    my_map[1] = Movie("SpiderMan",2001,4.33)
+    my_map[2] = Movie("Harry Potter",2006,4.2)
+    my_map[3] = Movie("James Bond",1999,5.0)
+    my_map[4] = Movie("Rush Hour",1999,4.5)
+    my_map[5] = Movie("SpiderMan2",2001,4.34)
     return my_map
 
 def find_highest_rated_movie(movies_dict):
-    movie_id=0
+    highest_rated_movie_id=0
     highest_rating=0
     
-    for line in movies_dict:
-        if movies_dict[line][2]>highest_rating:
-            highest_rating=movies_dict[line][2]
-            movie_id = line
-    return movie_id
+    for movieid, movie in movies_dict.items():
+        if movie.rating > highest_rating:
+            highest_rating = movie.rating
+            highest_rated_movie_id = movieid
+    return highest_rated_movie_id
 
 def find_highest_rated_movie_by_year(movies_dict):
-    rated_by_year_map={}
+    rating_by_year_map={}
+    
+    for line in movies_dict:
+        if movies_dict[line][1] in rating_by_year_map:
+            if movies_dict[line][2] > movies_dict[rating_by_year_map[movies_dict[line][1]] ][2]:
+                rating_by_year_map[movies_dict[line][1]] = line
+        else :
+            rating_by_year_map[movies_dict[line][1]]=line
+    return rating_by_year_map
     
 
 #Movie Data File Handling ends here.
