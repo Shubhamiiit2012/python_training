0cf4e21614d04eeca0ff9f5a486c1c26cedf824f
Shubham Lakhiwal <shubham.lakhiwal@hashedin.com>
2012-12-31 10:36:29 +0530

    Warm up Problems on Actual data done.

--- movie_map.py
+++ movie_map.py
@@ -1,5 +1,5 @@
 
-def get_movie_dict_other(movie_file):
+def get_movie_dict_other(movie_file, rating_file):
     movies_dict={}
     fin=open(movie_file)
     for line in fin:
@@ -21,15 +21,19 @@ def get_movie_dict_other(movie_file):
         
         #print(tokken[4])
         movies_dict[int(tokken[0])]=[name_tokken[0],movie_year,0,0,0,genre_list]
-    fin=open("ratings.data")
+    fin=open(rating_file)
     user_activity_map={}
     for line in fin:
         rating_tokken=line.split("\t")
         movies_dict[int(rating_tokken[1])][2]+=int(rating_tokken[2])
         movies_dict[int(rating_tokken[1])][3]+=1
+        if int(rating_tokken[0]) in user_activity_map:
+            user_activity_map[int(rating_tokken[0])]+=1
+        else :
+            user_activity_map[int(rating_tokken[0])]=1
         
     for movie_id in movies_dict:
         if movies_dict[movie_id][3] != 0:
             movies_dict[movie_id][4]=float(movies_dict[movie_id][2])/movies_dict[movie_id][3]
         
-    return movies_dict
+    return (movies_dict, user_activity_map )
